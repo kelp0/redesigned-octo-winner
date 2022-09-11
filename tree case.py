4da9af5295950e6296dda6e10beb0527b486ad71
kelp0 <93107543+kelp0@users.noreply.github.com>
2022-09-12 02:51:35 +0700

    Update tree case.py

--- tree case.py
+++ tree case.py
@@ -1,2 +1,39 @@
-tree = {'Живое?': {'да' : 'Мяукает?', 'нет' : 'Изделие?'}, 'Мяукает?' : {'да' :"Кошка", 'нет':"Собака"}, 'Изделие?': {'да' : "Стол", 'нет' : "Дерево"}, 'Кошка': None, 'Собака' : None, 'Дерево' : None, 'Стол' : None}
-print(tree['Живое?'])
+#Работу выполнили: Скороходов М. 50%, Лысенко М. 50%, Ячин Д. 40%.
+
+import json
+
+with open('case.json','r') as treefile: #Импорт начального дерева.
+    tree = json.load(treefile)
+
+def tree_edit(tree): #Рекурсивная функция, пробегает по вложенным словарям.
+    possible_answers = ['yes','no']
+    print(tree['question'])
+    a = input('Ответом может являться Yes или No: ').lower()
+    while a not in possible_answers: #Запрос ответа на вопрос, обработка ошибок.
+        a = input('Такого ответа не предусмотрено. Ответом является Yes или No: ').lower()
+
+    if type(tree[a]) == dict: #Проверка типа элемента.
+        tree_edit(tree[a])
+
+    elif type(tree[a]) == str:
+        print('Вы загадали: ', tree[a])
+        reality = input('Ответ удовлетворяет вашим ожиданиям? (Введите Yes или No): ').lower() #Добавление новых вопросов в случае необходимости.
+        while reality not in possible_answers:
+             reality = input('Такого ответа не предусмотрено. Ответом является Yes или No: ').lower()
+        if reality == 'yes':
+            print('Благодарим вас за то, что выбрали нашу программу!')
+        elif reality == 'no':
+            question = input('Какой вопрос помог бы нам отгадать то, что вы загадали?: ')
+            new = {'question':question,'yes':None,'no':None}
+            print('Спасибо, что помогаете нам улучшать эту программу!')
+            tree[a] = new
+            
+    elif tree[a] is None: #Добавление новго ответа.
+        new = input('Наши алгоритмы не в силах найти правильный ответ. Назовите его: ')
+        print('Спасибо, что помогаете нам улучшать эту программу!')
+        tree[a] = new
+
+tree_edit(tree) #Экспорт полученного дерева.
+with open('case.json','w') as treefile:
+    json.dump(tree, treefile, indent=3)
+        
